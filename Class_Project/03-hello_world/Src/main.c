4bcace593fee94933e4ffc2407fdc947114ae229
SMore <sagarmore1007@gmail.com>
2025-12-27 20:31:25 +0530

    ARM theory

---
+++ Class_Project/03-hello_world/Src/main.c
@@ -0,0 +1,22 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Sagar More
+ * @brief          : Main program body
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+
+
+	while(1){
+		printf("Hello World\r\n");
+		for(int i =0; i < 1000000; i++){}
+	}
+
+}
